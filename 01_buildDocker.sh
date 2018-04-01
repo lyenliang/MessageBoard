6a96b5a07b17f03012bf6b61a63e57fb95e321d2
lyenliang <lyenliang@gmail.com>
2018-04-01 11:03:38 +0800

    Update provision code

--- 01_buildDocker.sh
+++ 01_buildDocker.sh
@@ -2,8 +2,9 @@
 
 REPOSITORY=663601278082.dkr.ecr.ap-northeast-1.amazonaws.com/message-board/develop
 
-TAG=1.0.0
+TAG=1.1.0
 REGION=ap-northeast-1
 DOCKERFILE_PATH=provision/docker/Dockerfile
 
 docker build -t ${REPOSITORY}:${TAG} -f ${DOCKERFILE_PATH} .
+docker tag ${REPOSITORY}:${TAG} ${REPOSITORY}:latest
